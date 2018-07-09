69deb8b86836c16f1bbee79c16536cc398440e69
Stephen Elliott <stephen@bealine.co.uk>
2018-07-09 15:43:43 +0100

    BBB-206 Make transactional

--- src/main/java/uk/gov/dft/bluebadge/service/message/service/MessageService.java
+++ src/main/java/uk/gov/dft/bluebadge/service/message/service/MessageService.java
@@ -4,11 +4,13 @@ import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import uk.gov.dft.bluebadge.model.message.generated.MessageDetails;
 import uk.gov.dft.bluebadge.service.message.repository.MessageRepository;
 import uk.gov.dft.bluebadge.service.message.repository.domain.MessageEntity;
 
 @Service
+@Transactional
 @Slf4j
 public class MessageService {
 
