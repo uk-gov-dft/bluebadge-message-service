54b2da2ec3140850a71ca16330fab6abfa36576c
Stephen Elliott <stephen@bealine.co.uk>
2018-07-12 12:19:01 +0100

    BBB-185 Drop the old table and add schema name

--- database-schema/migrations/scripts/20180706110123_BBB-206-create-message-table.sql
+++ database-schema/migrations/scripts/20180706110123_BBB-206-create-message-table.sql
@@ -1,7 +1,8 @@
 --// BBB-206-create-message-table
 -- Migration SQL that makes the change goes here.
+DROP TABLE IF EXISTS message.password_reset_email;
 
-CREATE TABLE message(
+CREATE TABLE message.message(
   bbb_reference UUID NOT NULL,
   template VARCHAR (150),
   notify_reference UUID NOT NULL,
@@ -13,5 +14,5 @@ CREATE TABLE message(
 --//@UNDO
 -- SQL to undo the change goes here.
 
-DROP TABLE message;
+DROP TABLE message.message;
 
