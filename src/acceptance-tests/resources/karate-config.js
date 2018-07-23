9db309aa7a375ef5d1c13ebbc1a7cf7ab7b10574
Stephen Elliott <stephen@bealine.co.uk>
2018-07-23 11:06:01 +0100

    BBB-586 Karate tests with oauth

--- src/acceptance-tests/resources/karate-config.js
+++ src/acceptance-tests/resources/karate-config.js
@@ -1,6 +1,7 @@
 function() {
   return {
     baseUrl: 'http://localhost:8480/uk-gov-dft/message-service/1.0.0',
-    managementBaseUrl: 'http://localhost:8481/manage'
+    managementBaseUrl: 'http://localhost:8481/manage',
+    authServerUrl: 'http://localhost:8380/bb_auth'
   }
 }
